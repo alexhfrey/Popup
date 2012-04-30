530de3c4a2fa92e4251ac6018c66ac734ad830ed
alexhfrey <alex.h.frey@gmail.com>
2012-04-30 10:40:49 -0700

    dlkj

--- config/application.rb
+++ config/application.rb
@@ -38,7 +38,7 @@ module Popup
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
-
+    config.assets.initialize_on_precompile = false
     # Enable the asset pipeline
     config.assets.enabled = true
 
