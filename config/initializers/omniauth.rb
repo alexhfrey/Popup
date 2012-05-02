6cf273ec3c2c8a968b6d87c15a28a3c0567082cd
alexhfrey <alex.h.frey@gmail.com>
2012-05-02 10:46:52 -0700

    lad

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,19 +1,19 @@
 if Rails.env.production?  
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :facebook, '173481316095568', 'a106992ae684af007857e95876a8172a', :scope => 'email, offline_access, publish_stream' 	
+	provider :facebook, '122628081204967', 'c3daa02c78ef4475dd74d26560a9870c', :scope => 'email, offline_access' 	
 	
 	
   
 	{:client_options => {:ssl => {:ca_path => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
 	end
 if Rails.env.staging?
-	provider :facebook, '333924066629604', '0cc1164e4805256031da55dbb9740125', :scope => 'email, offline_access, publish_stream' 	
+	provider :facebook, '333924066629604', '0cc1164e4805256031da55dbb9740125', :scope => 'email, offline_access' 	
 	
 end  
 else
 
 	Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :facebook, '333924066629604', '0cc1164e4805256031da55dbb9740125', :scope => 'email, offline_access, publish_stream'  
+	provider :facebook, '333924066629604', '0cc1164e4805256031da55dbb9740125', :scope => 'email, offline_access'  
 
 end
 end
